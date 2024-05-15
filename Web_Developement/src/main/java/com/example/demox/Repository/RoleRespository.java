bfc3de21657d746b6af2e144048ca319b700b1db
Kushal <32552061+kushalkhanal@users.noreply.github.com>
2024-05-15 22:32:19 +0545

    Delete Web_Developement directory

--- Web_Developement/src/main/java/com/example/demox/Repository/RoleRespository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demox.Repository;
-
-import com.example.demox.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRespository extends JpaRepository<Role, Integer> {
-
-}
